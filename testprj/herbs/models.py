3c233da59c5c49bb9a54c42b7462ff83931c2a78
Kislov Dmitry <kislov@easydan.com>
2016-08-17 13:02:21 +1000

    translations of models

--- testprj/herbs/models.py
+++ testprj/herbs/models.py
@@ -1,4 +1,4 @@
-
+#coding: utf-8
 from imagekit.models import ProcessedImageField
 from django.utils.text import capfirst
 from django.db import models
@@ -33,17 +33,17 @@ class MetaDataMixin(models.Model):
     '''
     Common item properties
     '''
-    created = models.DateField(auto_now_add=True)
-    updated = models.DateField(auto_now=True)
+    created = models.DateField(auto_now_add=True, verbose_name=_('создан'))
+    updated = models.DateField(auto_now=True, verbose_name=_('сохранен'))
     createdby = models.ForeignKey(settings.AUTH_USER_MODEL,
                                   on_delete=models.SET_NULL,
                                   null=True, blank=True, related_name='+',
-                                  editable=False)
+                                  editable=False, verbose_name=_('создатель'))
     updatedby = models.ForeignKey(settings.AUTH_USER_MODEL,
                                   on_delete=models.SET_NULL,
                                   null=True, blank=True, related_name='+',
-                                  editable=False)
-    public = models.BooleanField(default=False)
+                                  editable=False, verbose_name=_('обновил'))
+    public = models.BooleanField(default=False, verbose_name=_('опубликовано'))
     
     class Meta:
         abstract = True 
@@ -53,7 +53,7 @@ class MetaDataMixin(models.Model):
 class Author(models.Model):
     '''Genus/Family/Species inventor
     '''
-    name = models.CharField(max_length=150, default='')
+    name = models.CharField(max_length=150, default='', verbose_name=_('автор'))
 
     def save(self, *args, **kwargs):
         self.name = self.name.lower().strip()
@@ -65,15 +65,20 @@ class Author(models.Model):
     def get_name(self):
         return capfirst(self.name)
 
+    class Meta:
+        verbose_name = _('автор')
+        verbose_name_plural = _('авторы')
 
 @python_2_unicode_compatible
 class FamilyAuthorship(models.Model):
     author = models.ForeignKey(Author,
                                null=True,
                                on_delete=models.CASCADE,
-                               blank=True)
-    family = models.ForeignKey('Family', on_delete=models.CASCADE)
-    priority = models.IntegerField(default=0)
+                               blank=True,
+                               verbose_name=_('автор'))
+    family = models.ForeignKey('Family', on_delete=models.CASCADE, verbose_name=_('семейство'))
+    priority = models.IntegerField(default=0, verbose_name=_('приоритет'), 
+                                   help_text=_('низкий приоритет  соответствует более старой номенклатуре'))
 
     def __str__(self):
         return str(self.author) + (' %s' % self.priority if self.priority > 0 else '') 
@@ -81,21 +86,30 @@ class FamilyAuthorship(models.Model):
     def get_name(self):
         return capfirst(self.author.name)
 
+    class Meta:
+        verbose_name = _('авторство')
+        verbose_name_plural = _('авторство')
 
 @python_2_unicode_compatible
 class GenusAuthorship(models.Model):
     author = models.ForeignKey(Author,
                                null=True,
                                on_delete=models.CASCADE,
-                               blank=True)
-    genus = models.ForeignKey('Genus', on_delete=models.CASCADE)
-    priority = models.IntegerField(default=0)
+                               blank=True,
+                               verbose_name=_('автор'))
+    genus = models.ForeignKey('Genus', on_delete=models.CASCADE,
+                              verbose_name=_('род'))
+    priority = models.IntegerField(default=0, verbose_name=_('приоритет'))
 
     def __str__(self):
         return str(self.author) + (' %s' % self.priority if self.priority > 0 else '') 
 
     def get_name(self):
         return capfirst(self.author.name)
+
+    class Meta:
+        verbose_name = _('авторство')
+        verbose_name_plural = _('авторство')
 
 
 @python_2_unicode_compatible
@@ -103,9 +117,11 @@ class SpeciesAuthorship(models.Model):
     author = models.ForeignKey(Author,
                                null=True,
                                on_delete=models.CASCADE,
-                               blank=True)
-    herbitem = models.ForeignKey('HerbItem', on_delete=models.CASCADE)
-    priority = models.IntegerField(default=0)
+                               blank=True,
+                               verbose_name=_('автор'))
+    herbitem = models.ForeignKey('HerbItem', on_delete=models.CASCADE,
+                                 verbose_name=_('гербарный образец'))
+    priority = models.IntegerField(default=0, verbose_name=_('приоритет'))
 
     def __str__(self):
         return str(self.author) + (' %s' % self.priority if self.priority > 0 else '') 
@@ -113,11 +129,17 @@ class SpeciesAuthorship(models.Model):
     def get_name(self):
         return capfirst(self.author.name)
 
+    class Meta:
+        verbose_name = _('авторство')
+        verbose_name_plural = _('авторство')
 
 @python_2_unicode_compatible
 class Family(models.Model):
-    name = models.CharField(max_length=30, default='')
-    authorship = models.ManyToManyField(Author, blank=True, null=True, through=FamilyAuthorship)
+    name = models.CharField(max_length=30, default='',
+                            verbose_name=_('название'))
+    authorship = models.ManyToManyField(Author, blank=True, null=True,
+                                        through=FamilyAuthorship,
+                                        verbose_name=_('авторство'))
 
     def save(self, *args, **kwargs):
         self.name = self.name.strip().lower()
@@ -133,12 +155,16 @@ class Family(models.Model):
         else:
             author_string = ''
         return self.name + author_string
-
+    get_full_name.short_description = _('полное имя семейства')
+    
+    class Meta:
+        verbose_name = _('название семейства')
+        verbose_name_plural = _('названия семейств')    
 
 @python_2_unicode_compatible
 class Genus(models.Model):
-    name = models.CharField(max_length=30, default='')
-    authorship = models.ManyToManyField(Author, blank=True, null=True, through=GenusAuthorship)
+    name = models.CharField(max_length=30, default='', verbose_name=_('название'))
+    authorship = models.ManyToManyField(Author, blank=True, null=True, through=GenusAuthorship, verbose_name=_('авторство'))
     
     def save(self, *args, **kwargs):
         self.name = self.name.strip().lower()
@@ -154,6 +180,11 @@ class Genus(models.Model):
         else:
             author_string = ''
         return self.name + author_string
+    get_full_name.short_description = _('полное имя рода')
+
+    class Meta:
+        verbose_name = _('название рода')
+        verbose_name_plural = _('названия родов')
 
 
 @python_2_unicode_compatible
@@ -165,6 +196,10 @@ class Species(models.Model):
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name = _('название вида')
+        verbose_name_plural = _('названия видов')
 
 
 class HerbSnapshot(models.Model):
@@ -177,33 +212,39 @@ class HerbSnapshot(models.Model):
 
 @python_2_unicode_compatible
 class HerbItem(MetaDataMixin):
-    family = models.ForeignKey(Family, on_delete=models.SET_NULL, null=True)
-    genus = models.ForeignKey(Genus, on_delete=models.SET_NULL, null=True)
-    species = models.ForeignKey(Species, on_delete=models.SET_NULL, null=True)
+    family = models.ForeignKey(Family,
+                               on_delete=models.SET_NULL,
+                               null=True,
+                               verbose_name=_('семейство'))
+    genus = models.ForeignKey(Genus, on_delete=models.SET_NULL, null=True,
+                              verbose_name=_('род'))
+    species = models.ForeignKey(Species, on_delete=models.SET_NULL, null=True,
+                                verbose_name=_('вид'))
     authorship = models.ManyToManyField(Author, blank=True,
-                                        null=True, through=SpeciesAuthorship)
+                                        null=True, through=SpeciesAuthorship,
+                                        verbose_name=_('авторство'))
 
     # item specific codes (used in the herbarium store)
-    gcode = models.CharField(max_length=10, default='')
-    itemcode = models.CharField(max_length=15, default='')
+    gcode = models.CharField(max_length=10, default='', verbose_name=_('код подраздела'))
+    itemcode = models.CharField(max_length=15, default='', verbose_name=_('код образца'))
 
     # position
-    country = models.CharField(default='', blank=True, max_length=2)
-    region = models.CharField(default='', blank=True, max_length=150)
-    district = models.CharField(default='', blank=True, max_length=150)
-    detailed = models.CharField(default='', max_length=300, blank=True)
-    place = GeopositionField()
+    country = models.CharField(default='', blank=True, max_length=2, verbose_name=_('страна'))
+    region = models.CharField(default='', blank=True, max_length=150, verbose_name=_('регион'))
+    district = models.CharField(default='', blank=True, max_length=150, verbose_name=_('район'))
+    detailed = models.CharField(default='', max_length=300, blank=True, verbose_name=_('дополнительно'))
+    place = GeopositionField(verbose_name=_('координаты'))
 
     # Ecological factors
-    ecodescr = models.CharField(max_length=300, default='', blank=True)
+    ecodescr = models.CharField(max_length=300, default='', blank=True, verbose_name=_('экоусловия'))
 
     # Collection items
-    collectors = models.CharField(max_length=500, default='', blank=True) 
-    collected_s = models.DateField(blank=True)
-    collected_e = models.DateField(blank=True)
-    identifiers = models.CharField(max_length=500, default='', blank=True)
-    identified_s = models.DateField(blank=True)
-    identified_e = models.DateField(blank=True)
+    collectors = models.CharField(max_length=500, default='', blank=True, verbose_name=_('сборщики')) 
+    collected_s = models.DateField(blank=True, verbose_name=_('начало сбора'))
+    collected_e = models.DateField(blank=True, verbose_name=_('конец сбора'))
+    identifiers = models.CharField(max_length=500, default='', blank=True, verbose_name=_('определил(и)'))
+    identified_s = models.DateField(blank=True, verbose_name=_('начало определения'))
+    identified_e = models.DateField(blank=True, verbose_name=_('конец определения'))
 
     def save(self, *args, **kwargs):
         self.collectors = self.collectors.strip()
@@ -222,4 +263,11 @@ class HerbItem(MetaDataMixin):
         else:
             author_string = ''
         return capfirst(self.genus.name) + ' ' + self.species.name + author_string
+    get_full_name.short_description = _('полное имя вида')
+    
+    class Meta:
+        abstract = False
+        verbose_name = _('гербарный образeц')
+        verbose_name_plural = _('гербарные образцы')
+    
     
