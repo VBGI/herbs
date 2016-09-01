cb987b38ad94dbc741dda478d0a5d300f7e628d7
Kislov Dmitry <kislov@easydan.com>
2016-09-01 13:28:49 +1000

    Integration with django-cms

---
+++ testprj/herbs/cms_plugins.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from cms.plugin_base import CMSPluginBase
+from cms.models.pluginmodel import CMSPlugin
+from cms.plugin_pool import plugin_pool
+from django.utils.translation import ugettext_lazy as _
+
+
+class PrintHerbitemObjects(CMSPluginBase):
+    model=CMSPlugin
+    name = _(u"Отображение гербарных образцов")
+    render_template="render_herb_plugin.html"
+    def render(self, context, instance, placeholder):
+        return context
+    
+plugin_pool.register_plugin(PrintHerbitemObjects)    
