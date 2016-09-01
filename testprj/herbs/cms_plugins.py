44c1ba3fe3dcc41f1934fab4489e45e6a3e4c313
Kislov Dmitry <kislov@easydan.com>
2016-09-01 14:57:38 +1000

    Invalid comments in template

--- testprj/herbs/cms_plugins.py
+++ testprj/herbs/cms_plugins.py
@@ -3,6 +3,7 @@ from cms.plugin_base import CMSPluginBase
 from cms.models.pluginmodel import CMSPlugin
 from cms.plugin_pool import plugin_pool
 from django.utils.translation import ugettext_lazy as _
+from .forms import SearchForm
 
 
 class PrintHerbitemObjects(CMSPluginBase):
@@ -10,6 +11,7 @@ class PrintHerbitemObjects(CMSPluginBase):
     name = _(u"Отображение гербарных образцов")
     render_template="render_herb_plugin.html"
     def render(self, context, instance, placeholder):
+        context.update({'searchform': SearchForm()})
         return context
     
 plugin_pool.register_plugin(PrintHerbitemObjects)    
--- testprj/herbs/urls.py
+++ testprj/herbs/urls.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import *
-from .views import get_item_data, show_herbs, advice_select
 
 urlpatterns = patterns('',
-   url(r'^gi/', get_item_data),
-   url(r'^as/', advice_select),
-   url(r'^sh/', show_herbs),
+   url(r'^gi/', 'herbs.views.get_item_data'),
+   url(r'^as/', 'herbs.views.advice_select'),
+   url(r'^sh/', 'herbs.views.show_herbs'),
 )
