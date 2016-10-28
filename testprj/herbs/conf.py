aa5ee7745af99a7f60d9fc48d29dd8700f0aa141
Kislov Dmitry <kislov@easydan.com>
2016-10-28 13:00:44 +1000

    Pagination changed

--- testprj/herbs/conf.py
+++ testprj/herbs/conf.py
@@ -1,9 +1,11 @@
 from django.conf import settings
+
 from appconf import AppConf
 
+
 class HerbsAppConf(AppConf):
-    PAGINATION_COUNT = 100
+    PAGINATION_COUNT = 20
     AUTOSUGGEST_NUM_TO_SHOW = 50
-    
+
     class Meta:
         prefix = 'herbs'
