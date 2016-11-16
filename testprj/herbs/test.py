d941e50810e805e69cecf82cf55cc7cc1349f2ff
Kislov Dmitry <kislov@easydan.com>
2016-11-16 16:28:58 +1000

    Refactoring needed

--- testprj/herbs/test.py
+++ testprj/herbs/test.py
@@ -34,99 +34,98 @@ goto = lambda n: FIRST_LINE + LINE_HEIGHT * n
 class PDF_DOC:
     def __init__(self):
         self.pdf = FPDF(orientation='L')
-        pdf.add_font('DejaVu', '', 'DejaVuSansCondensed.ttf', uni=True)
-        pdf.add_font('DejaVui', '', 'DejaVuSans-Oblique.ttf', uni=True)
+        self.pdf.add_font('DejaVu', '', 'DejaVuSansCondensed.ttf', uni=True)
+        self.pdf.add_font('DejaVui', '', 'DejaVuSans-Oblique.ttf', uni=True)
 
-    def _add_label(self, x, y, family=''):
-        pdf.rect(x, y, LABEL_WIDTH,LABEL_HEIGHT, '')
-        pdf.rect(x-3, y-3, LABEL_WIDTH+6, LABEL_HEIGHT+6, '')
-        pdf.set_xy(x+5, y+5)
-        pdf.image('./imgs/bgi_logo.png', w=15, h=15)
-        pdf.set_font('DejaVu', '', 14)
-        pdf.set_xy(x+20, goto(0))
-        pdf.cell(LABEL_WIDTH - 20, 0, msgs['org'], align='C')
-        pdf.set_font_size(10)
-        pdf.set_xy(x+20, goto(1))
-        pdf.cell(LABEL_WIDTH - 20,0, msgs['addr'],align='C')
-        pdf.set_xy(x+20, goto(2))
-        pdf.cell(LABEL_WIDTH - 20, 0,msgs['descr'], align='C')
-        pdf.line(x + 5, goto(2) + 4,LABEL_WIDTH + 5 - 2 ,goto(2) + 4)
-        pdf.set_xy(x + 5, goto(3))
-        pdf.set_font('DejaVui', '', 12)
-        if family_name:
-            pdf.cell(LABEL_WIDTH, 0, '{}'.format(family_name), align='C')
+    def _add_label(self, x, y, family='', species='', spauth1='', spauth2='',
+                   date=''):
+        self.pdf.rect(x, y, LABEL_WIDTH,LABEL_HEIGHT, '')
+        self.pdf.rect(x - 3, y - 3, LABEL_WIDTH + 6, LABEL_HEIGHT + 6, '')
+        self.pdf.set_xy(x + 5, y + 5)
+        self.pdf.image('./imgs/bgi_logo.png', w=15, h=15)
+        self.pdf.set_font('DejaVu', '', 14)
+        self.pdf.set_xy(x + 20, goto(0))
+        self.pdf.cell(LABEL_WIDTH - 20, 0, msgs['org'], align='C')
+        self.pdf.set_font_size(10)
+        self.pdf.set_xy(x + 20, goto(1))
+        self.pdf.cell(LABEL_WIDTH - 20,0, msgs['addr'],align='C')
+        self.pdf.set_xy(x + 20, goto(2))
+        self.pdf.cell(LABEL_WIDTH - 20, 0,msgs['descr'], align='C')
+        self.pdf.line(x + 5, goto(2) + 4,LABEL_WIDTH + 5 - 2 ,goto(2) + 4)
+        self.pdf.set_xy(x + 5, goto(3))
+        self.pdf.set_font('DejaVui', '', 12)
+        if family:
+            self.pdf.cell(LABEL_WIDTH, 0, '{}'.format(family), align='C')
         else:
-            pdf.cell(LABEL_WIDTH, 0, '_____________________', align='C')
+            self.pdf.cell(LABEL_WIDTH, 0, '_____________________', align='C')
 
         # -------------- Plot Genus name ---------------
-        pdf.set_xy(5, goto(4))
-        species_name = 'Some species'
-        author_name = '(Sec. Author) Prim. Author'
-        pdf.set_font_size(10)
-        sp_w = pdf.get_string_width(species_name)
-        pdf.set_font('DejaVu', '', 10)
-        au_w = pdf.get_string_width(author_name)
-        pdf.set_xy(LABEL_WIDTH/2-(au_w+sp_w+2)/2, goto(4))
-        pdf.set_font('DejaVui', '', 10)
-        pdf.set_font_size(10)
-        pdf.cell(0,0, '{}'.format(species_name))
-        pdf.set_font('DejaVu', '', 10)
-        pdf.set_xy(LABEL_WIDTH/2-(au_w+sp_w+2)/2 + sp_w + 2, goto(4))
-        pdf.cell(0,0,'{}'.format(author_name))
+        self.pdf.set_xy(x + 5, goto(4))
+        species_name = species
+        author_name = ''
+        author_name += '(%s)' % spauth2 if spauth2 else ''
+        author_name += ' %s' % spauth1 if spauth1 else ''
+        self.pdf.set_font_size(11)
+        sp_w = self.pdf.get_string_width(species_name)
+        self.pdf.set_font('DejaVu', '', 11)
+        au_w = self.pdf.get_string_width(author_name)
+        self.pdf.set_xy(LABEL_WIDTH/2-(au_w+sp_w+2)/2, goto(4))
+        self.pdf.set_font('DejaVui', '', 11)
+        self.pdf.set_font_size(11)
+        self.pdf.cell(0,0, '{}'.format(species_name))
+        self.pdf.set_font('DejaVu', '', 11)
+        self.pdf.set_xy(LABEL_WIDTH/2-(au_w+sp_w+2)/2 + sp_w + 2, goto(4))
+        self.pdf.cell(0,0,'{}'.format(author_name))
         # ----------------------------------------------
 
 
 
+        # ------------- place of collecting ------------
+
+        self.pdf.set_xy(5, goto(5))
+        self.pdf.set_font('DejaVu', '', 9)
+        tw = self.pdf.get_string_width(msgs['where'])
+        self.pdf.cell(0, 0, msgs['where'])
+        self.pdf.set_xy(x + 5 + 1 + tw, goto(5))
+        self.pdf.cell(0, 0, place_short)
+        self.pdf.set_xy(x + 5, goto(6))
+        self.pdf.cell(LABEL_WIDTH, 0, place_long, align='C')
+
+        # ----------------------------------------------
+
+        # ------------- Coordinates found -------------
+
+        self.pdf.set_xy(x + 5, goto(7))
+        tw = self.pdf.get_string_width(msgs['coords'])
+        self.pdf.cell(0, 0, msgs['coords'])
+
+        if self.latitude and self.longitude:
+            self.pdf.set_xy(x + 5 + 1 + tw, goto(7))
+            self.pdf.cell(0, 0, 'LAT=' + str(latitude) + u'\N{DEGREE SIGN}'+\
+                          ' LON=' + str(longitude) + u'\N{DEGREE SIGN}')
+
+        # ----------------------------------------------
+
+        # ----------- Date found -----------------------
+        self.pdf.set_xy(x + 5, goto(8))
+        self.pdf.cell(0, 0, msgs['date'])
+        tw = self.pdf.get_string_width(msgs['date'])
+        self.pdf.set_xy(x + 5 + 1 + tw, goto(8))
+        self.pdf.cell(0, 0, str(date))
 
 
+        self.df.set_xy(x + 5, goto(9))
+        self.pdf.cell(0, 0, msgs['det'])
+        tw = self.pdf.get_string_width(msgs['det'])
+        self.pdf.set_xy(x + 5 + 1 + tw, goto(9))
 
+        for k in identified.split():
+            pass
+        self.pdf.cell(0,0, iden)
 
-
-
-
-
-
-
-# ------------- place of collecting ------------
-pdf.set_xy(5,goto(5))
-pdf.set_font('DejaVu', '', 9)
-tw = pdf.get_string_width(msgs['where'])
-pdf.cell(0, 0, msgs['where'])
-pdf.set_xy(5 + 1 + tw, goto(5))
-pdf.cell(0, 0, 'Россия, Приморский край, с.xxxx')
-
-
-pdf.set_xy(5, goto(6))
-pdf.cell(LABEL_WIDTH, 0, 'более длинное описание места сбора...', align='C')
-# ----------------------------------------------
-
-
-# ------------- Coordinates found -------------
-
-pdf.set_xy(5, goto(7))
-tw = pdf.get_string_width(msgs['coords'])
-pdf.cell(0,0, msgs['coords'])
-pdf.set_xy(5+1+tw, goto(7))
-pdf.cell(0,0, 'LAT=12.2390'+u'\N{DEGREE SIGN}'+', '+'LON=123.399312'+u'\N{DEGREE SIGN}')
-
-
-#-----------------------------------------------
-
-pdf.set_xy(5, goto(8))
-pdf.cell(0,0,msgs['date'])
-tw=pdf.get_string_width(msgs['date'])
-pdf.set_xy(5+1+tw, goto(8))
-pdf.cell(0,0,'12 Июля 2000 г.')
-
-pdf.set_xy(5, goto(9))
-pdf.cell(0,0,msgs['det'])
-tw=pdf.get_string_width(msgs['det'])
-pdf.set_xy(5+1+tw, goto(9))
-pdf.cell(0,0,'Иванов И.И., Петров А.А. ')
-
-pdf.set_font_size(12)
-pdf.set_xy(15,goto(11))
-pdf.cell(0,0,'Инв. №: 445511:1122')
+        self.pdf.set_font_size(12)
+        self.pdf.set_xy(x + 15, goto(11))
+        self.pdf.cell(0,0,'Инв. №: %s' % number)
 
 
 
