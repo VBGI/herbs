a2b666eaa44a24f88c6590ebf90099b306636b37
Kislov Dmitry <kislov@easydan.com>
2016-11-22 11:09:48 +1000

    Make label route added

--- testprj/herbs/urls.py
+++ testprj/herbs/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
    url(r'^gi/', 'herbs.views.get_item_data'),
    url(r'^as/', 'herbs.views.advice_select'),
    url(r'^sh/', 'herbs.views.show_herbs'),
+   url(r'^pdf/([,\d]+)', 'herbs.views.make_label')
 )
