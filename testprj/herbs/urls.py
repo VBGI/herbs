9dc10b84ca029a68ad396a1fdfa9020a0cbfe7db
Kislov Dmitry <kislov@easydan.com>
2016-11-24 10:45:37 +1000

    urls tweaked for pdf action

--- testprj/herbs/urls.py
+++ testprj/herbs/urls.py
@@ -2,9 +2,12 @@
 
 from django.conf.urls import *
 
+from herbs.views import (get_item_data, advice_select, show_herbs,
+                         make_label)
+
 urlpatterns = patterns('',
-   url(r'^gi/', 'herbs.views.get_item_data'),
-   url(r'^as/', 'herbs.views.advice_select'),
-   url(r'^sh/', 'herbs.views.show_herbs'),
-   url(r'^([,\d]{1,50})', 'herbs.views.make_label')
+   url(r'^gi/', get_item_data),
+   url(r'^as/', advice_select),
+   url(r'^sh/', show_herbs),
+   url(r'^pdf/([,\d]{1,50})', make_label, name='herbiteminfo')
 )
