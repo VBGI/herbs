61c56e64b0ea058f457eb9b9177d7e02ea29887d
Kislov Dmitry <kislov@easydan.com>
2016-11-29 11:06:39 +1000

    Admin autosuggest : the num to show

--- testprj/herbs/conf.py
+++ testprj/herbs/conf.py
@@ -6,6 +6,7 @@ from appconf import AppConf
 class HerbsAppConf(AppConf):
     PAGINATION_COUNT = 20
     AUTOSUGGEST_NUM_TO_SHOW = 50
+    AUTOSUGGEST_NUM_ADMIN = 30
 
     class Meta:
         prefix = 'herbs'
--- testprj/herbs/lookups.py
+++ testprj/herbs/lookups.py
@@ -4,38 +4,40 @@ from .countries import codes as ccodes
 from django.utils.encoding import force_text
 from django.utils.safestring import mark_safe
 from django.utils.html import escape
+from .conf import settings
 
+NS = settings.AUTOSUGGEST_NUM_ADMIN
 
 @register('family')
 class FamilyLookup(LookupChannel):
     model = Family
     def get_query(self, q, request):
-        return self.model.objects.filter(name__icontains=q).order_by('name')[:20]
+        return self.model.objects.filter(name__icontains=q).order_by('name')[:NS]
 
 @register('genus')
 class GenusLookup(LookupChannel):
     model = Genus
     def get_query(self, q, request):
-        return self.model.objects.filter(name__icontains=q).order_by('name')[:20]
+        return self.model.objects.filter(name__icontains=q).order_by('name')[:NS]
 
 @register('species')
 class SpeciesLookup(LookupChannel):
     model = Species
     def get_query(self, q, request):
-        return self.model.objects.filter(name__icontains=q).order_by('name').values('name').distinct()[:20]
+        return self.model.objects.filter(name__icontains=q).order_by('name').values_list('name').distinct()[:NS]
 
 @register('authorlookup')
 class AuthorLookup(LookupChannel):
     model = Author
     def get_query(self, q, request):
-        return self.model.objects.filter(name__icontains=q)[:20]
+        return self.model.objects.filter(name__icontains=q)[:NS]
 
 
 @register('country')
 class CountryLookup(LookupChannel):
     def get_query(self, q, request):
         res = [item for item in ccodes if q.lower() in item.decode('utf-8').lower()]
-        return res[:20]
+        return res[:NS]
 
     def format_match(self, obj):
         return obj
@@ -64,25 +66,25 @@ class DistinctValueMixin(LookupChannel):
 class RegionLookup(DistinctValueMixin):
     def get_query(self, q, request):
         return map(lambda x: x[0], HerbItem.objects.filter(region__icontains=q).order_by('updated',
-                                                                     'region').values_list('region').distinct())[:20]
+                                                                     'region').values_list('region').distinct())[:NS]
 
 @register('district')
 class DistrictLookup(LookupChannel):
     def get_query(self, q, request):
         return map(lambda x: x[0], HerbItem.objects.filter(district__icontains=q).order_by('updated',
-                                                                     'district').values_list('district').distinct())[:20]
+                                                                     'district').values_list('district').distinct())[:NS]
 
 
 @register('collectedby')
 class CollectorsLookup(LookupChannel):
     def get_query(self, q, request):
         return map(lambda x: x[0], HerbItem.objects.filter(collectedby__icontains=q).order_by('updated',
-                                                                     'collectedby').values_list('collectedby').distinct())[:20]
+                                                                     'collectedby').values_list('collectedby').distinct())[:NS]
 
 
 @register('identifiedby')
 class IdentifiersLookup(LookupChannel):
     def get_query(self, q, request):
         return map(lambda x: x[0], HerbItem.objects.filter(identifiedby__icontains=q).order_by('updated',
-                                                                     'identifiedby').values_list('identifiedby').distinct())[:20]
+                                                                     'identifiedby').values_list('identifiedby').distinct())[:NS]
 
