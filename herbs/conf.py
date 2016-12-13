af9f689e5ccedf28c9023f91a554131a78b6f9b8
Kislov Dmitry <kislov@easydan.com>
2016-12-13 14:03:38 +1000

    Autosuggest char settings added

--- herbs/conf.py
+++ herbs/conf.py
@@ -7,6 +7,7 @@ class HerbsAppConf(AppConf):
     PAGINATION_COUNT = 20
     AUTOSUGGEST_NUM_TO_SHOW = 50
     AUTOSUGGEST_NUM_ADMIN = 30
+    AUTOSUGGEST_CHAR = 3
 
     class Meta:
         prefix = 'herbs'
