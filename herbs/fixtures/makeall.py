81e14257b85abca7e4e99baf6ba32b7c8eafe1cd
Kislov Dmitry <kislov@easydan.com>
2016-12-15 14:53:27 +1000

    Some issues when species generated

--- herbs/fixtures/makeall.py
+++ herbs/fixtures/makeall.py
@@ -2,8 +2,12 @@
 from __future__ import print_function
 from .countries import *
 from ..models import Family, Genus, Species, Country
+from ..utils import  create_safely
+
 import gc
 import time
+import os
+
 
 CDIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -27,22 +31,17 @@ subprocess.call(['unzip', os.path.join(CDIR, 'species.zip'), '-d', CDIR])
 
 for file in os.listdir(CDIR):
     if file.endswith(".csv"):
-        data = pd.read_csv(file, dtype=pd.np.object)
+        data = pd.read_csv(os.path.join(CDIR, file), dtype=pd.np.object)
         for row in data.iterrows():
-            genus = row[-1]['Genus']
-            family = row[-1]['Family']
-            species = row[-1]['Species']
+            genus = row[-1]['Genus'].lower().strip()
+            family = row[-1]['Family'].lower().strip()
+            species = row[-1]['Species'].lower().strip()
             authorship = row[-1]['Authorship']
-            famobj = Family.objects.get_or_create(name=family.lower().strip())
-            genobj = Genus.objects.get_or_create(family=famobj,
-                                                 name=genus.lower().strip())
-            spobj = Species.objects.get_or_create(genus=genobj,
-                                                  name=species.lower().strip(),
-                                                  authorship=authorship)
+            famobj = create_safely(Family, ('name',), (family,))
+            genobj = create_safely(Genus, ('name','family__name'), (genus,family))
+            spobj = create_safely(Species, ('name', 'genus__name', 'authorship'), (species, genus, authorship))
             time.sleep(0.001)
         gc.collect()
 subprocess.call(['rm', os.path.join(CDIR, '*.csv')])
 
 # ---------- All data was loaded successfully --------------
-
-
