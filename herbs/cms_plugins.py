6162241fce53839a4f7597b457a23b5dc30ee419
Kislov Dmitry <kislov@easydan.com>
2017-04-14 12:45:34 +1000

    missing settings in cms_plugins

--- herbs/cms_plugins.py
+++ herbs/cms_plugins.py
@@ -4,7 +4,7 @@ from cms.models.pluginmodel import CMSPlugin
 from cms.plugin_pool import plugin_pool
 from django.utils.translation import ugettext_lazy as _
 from .forms import SearchForm
-
+from django.conf import settings
 
 class PrintHerbitemObjects(CMSPluginBase):
     model = CMSPlugin
@@ -12,7 +12,9 @@ class PrintHerbitemObjects(CMSPluginBase):
     render_template = "render_herb_plugin.html"
 
     def render(self, context, instance, placeholder):
-        context.update({'searchform': SearchForm()})
+        context.update({'searchform': SearchForm(),
+                        'herbitem_personal_url': settings.HERBS_HERBITEM_PAGE
+                        })
         return context
 
 plugin_pool.register_plugin(PrintHerbitemObjects)
