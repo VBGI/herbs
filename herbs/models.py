a6fe6d30156e18940d92dec7df7f60060c25f7bd
Kislov Dmitry <kislov@easydan.com>
2017-06-02 09:50:41 +1000

    Unnecessary func. removed

--- herbs/models.py
+++ herbs/models.py
@@ -1,18 +1,13 @@
 #coding: utf-8
 
-#import os
-
 from django.conf import settings
 from django.db import models
-#from django.db.models.signals import post_save, pre_delete
-#from django.dispatch import receiver
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.text import capfirst
 from django.utils.translation import gettext as _
 from django.contrib.auth import get_user_model
 from geoposition.fields import GeopositionField
 from django.core.exceptions import PermissionDenied
-#import pandas as pd
 from .utils import  _smartify_dates
 
 
@@ -360,149 +355,3 @@ class HerbItem(HerbItemMixin):
         else:
             super(HerbItem, self).delete(*args, **kwargs)
 
-
-# ------------------------ This functionalyty doesn't needed ----------------
-#class PendingHerbs(HerbItemMixin):
-#    checked = models.BooleanField(default=False, verbose_name=_('проверено'))
-#    err_msg = models.TextField(blank=True, default='')
-#
-#    class Meta:
-#        abstract = False
-#        verbose_name = _('загруженный гербарный образец')
-#        verbose_name_plural = _('загруженные гербарные образцы')
-#
-#
-#@python_2_unicode_compatible
-#class LoadedFiles(models.Model):
-#    datafile = models.FileField(upload_to=settings.HERB_DATA_UPLOADPATH, verbose_name=_('Файл'))
-#    created = models.DateField(auto_now_add=True, verbose_name=_('загружен'))
-#    status = models.BooleanField(default=False, editable=False, verbose_name=_('cтатус'))
-#    createdby = models.ForeignKey(settings.AUTH_USER_MODEL,
-#                                  on_delete=models.SET_NULL,
-#                                  null=True, blank=True, related_name='+',
-#                                  editable=False, verbose_name=_('создатель'))
-#
-#    def __str__(self):
-#        return os.path.basename(self.datafile.name)
-#
-#    class Meta:
-#        verbose_name = _('Файл с данными')
-#        verbose_name_plural = _('Файлы с данными')
-#        ordering = ('created', 'status', 'createdby')
-#
-#
-#@python_2_unicode_compatible
-#class ErrorLog(models.Model):
-#    created = models.DateTimeField(auto_now_add=True)
-#    message = models.TextField(blank=True, default='', editable=False)
-#    who = models.CharField(default='', blank=True, max_length=255)
-#
-#    def __str__(self):
-#        return self.message
-#
-#    class Meta:
-#        verbose_name = _('Ошибки загрузки файлов')
-#        verbose_name_plural = _('Ошибки загрузки файлов')
-#        ordering = ('-created', 'message')
-#
-#
-#@receiver(pre_delete, sender=LoadedFiles)
-#def loadedfiles_delete(sender, instance, **kwargs):
-#    try:
-#        instance.datafile.delete(False)
-#    except IOError:
-#        pass
-#
-#
-## NOTE: THIS FUNCTIONALITY DOESN"T NEEDED
-#@receiver(post_save, sender=LoadedFiles)
-#def load_datafile(sender, instance, **kwargs):
-#    # Trying
-#    herbfile = instance.datafile
-#    filename, file_extension = os.path.splitext(herbfile.name)
-#    fsize = herbfile.size
-#    if fsize > UPLOAD_MAX_FILE_SIZE:
-#        ErrorLog.objects.create(message=u'Превышен допустимый размер файла (%s байт), файл: %s' % (fsize, filename), who=filename)
-#        return
-#    if ('xls' in file_extension) or ('xlsx' in file_extension):
-#        try:
-#            data = pd.read_excel(os.path.join(settings.MEDIA_ROOT, herbfile.name))
-#        except:
-#            ErrorLog.objects.create(message=u'Не удалось прочитать файл %s' % (herbfile.name,), who=filename)
-#            return
-#    elif 'csv' in file_extension:
-#        try:
-#            data = pd.read_csv(os.path.join(settings.MEDIA_ROOT, herbfile.name))
-#        except:
-#            ErrorLog.objects.create(message=u'Не удалось прочитать файл %s' % (herbfile.name,), who=filename)
-#            return
-#    else:
-#        ErrorLog.objects.create(message=u'Неизвестный формат файла %s; Поддерживаемые форматы xls, csv.' % (herbfile.name,), who=filename)
-#        return
-#    ccolumns = set(data.columns)
-#    ncolumns = set(NECESSARY_DATA_COLUMNS)
-#    res = ncolumns - ccolumns
-#    if len(res) > 0:
-#        fields = ','.join(['<%s>'%item for item in res])
-#        errlog = ErrorLog(message=u'Поля %s отсутствуют в файле %s' % (fields, herbfile.name), who=filename)
-#        errlog.save()
-#        return
-#    result, errors = evluate_herb_dataframe(data)
-#    for err in errors:
-#        if err:
-#            ErrorLog.objects.create(message=';'.join([str(item) for item in err]), who=filename)
-#    if len(result) > 0:
-#        indd = 0
-#        for item in result:
-#            familyobj = create_safely(Family, ('name',), (item['family'],))
-#            if not familyobj.authorship.all().exists():
-#                for ind, auth in item['family_auth'][1]:
-#                    authorobj = create_safely(Author, ('name',), (auth,))
-#                    create_safely(FamilyAuthorship, ('author', 'priority', 'family'),
-#                              (authorobj, ind, familyobj), postamble='')
-#
-#            genusobj = create_safely(Genus, ('name', ), (item['genus'],))
-#            if not genusobj.family:
-#                genusobj.family = familyobj
-#            if not genusobj.gcode:
-#                genusobj.gcode = item['gcode']
-#            genusobj.save()
-#            if not genusobj.authorship.all().exists():
-#                for ind, auth in item['genus_auth'][1]:
-#                    authorobj = create_safely(Author, ('name',), (auth,))
-#                    create_safely(GenusAuthorship, ('author', 'priority', 'genus'),
-#                              (authorobj, ind, genusobj), postamble='')
-#
-#            speciesobj = create_safely(Species, ('name', 'genus'),
-#                                       (item['species'].strip().lower(), genusobj),
-#                                       postamble='')
-#            if not speciesobj.authorship.all().exists():
-#                for ind, auth in item['species_auth'][1]:
-#                    authorobj = create_safely(Author, ('name',), (auth,))
-#                    create_safely(SpeciesAuthorship, ('author', 'priority', 'species'),
-#                              (authorobj, ind, speciesobj), postamble='')
-#            query_fields = {'family': familyobj,
-#                            'genus': genusobj,
-#                            'species': speciesobj,
-#                            'itemcode': item['itemcode'],
-#                            'identified_s': item['identified'],
-#                            'identified_e': item['identified'],
-#                            'identifiedby': item['identifiedby'],
-#                            'collectedby': item['collectedby'],
-#                            'collected_s': item['collected'],
-#                            'collected_e': item['collected'],
-#                            'country': item['country'],
-#                            'region': item['region'],
-#                            'district': item['district'],
-#                            'coordinates': item['coordinates'],
-#                            'ecodescr': item['ecology'],
-#                            'detailed': item['detailed'],
-#                            'altitude': item['altitude'],
-#                            'note': item['note'],
-#                            }
-#            if not PendingHerbs.objects.filter(**query_fields).exists():
-#                pobj = PendingHerbs.objects.create(**query_fields)
-#                if HerbItem.objects.filter(itemcode=pobj.itemcode).exists():
-#                    pobj.err_msg += u'Запись с номером %s уже существует;' % pobj.itemcode
-#                    pobj.save()
-#            indd += 1
