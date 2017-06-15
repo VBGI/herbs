f01db80ad295679026f1bf432768f9ddc03f7a44
Kislov Dmitry <kislov@easydan.com>
2017-06-15 11:34:29 +1000

    Restrict the number of simultaneous conn to jsonapi

--- herbs/conf.py
+++ herbs/conf.py
@@ -13,5 +13,10 @@ class HerbsAppConf(AppConf):
     ALLOWED_FIELDID_SYMB_IN_GET = 30
     ALLOWED_AUTHORSHIP_SYMB_IN_GET = 100
     ALLOWED_ITEMCODE_SYMB_IN_GET = 30
+    
+    JSON_API_SIMULTANEOUS_CONN = 2
+    JSON_API_CONN_KEY_NAME = 'herbs_json_connections'
+    JSON_API_CONN_MAX_TIME = 3600
+
     class Meta:
         prefix = 'herbs'
