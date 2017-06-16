603ddb310670c70416cc4ca7ec0d3ffd7470b645
Kislov Dmitry <kislov@easydan.com>
2017-06-16 14:33:35 +1000

    CONN_KEY_FLAG introduced

--- herbs/conf.py
+++ herbs/conf.py
@@ -13,10 +13,11 @@ class HerbsAppConf(AppConf):
     ALLOWED_FIELDID_SYMB_IN_GET = 30
     ALLOWED_AUTHORSHIP_SYMB_IN_GET = 100
     ALLOWED_ITEMCODE_SYMB_IN_GET = 30
-    
+
     JSON_API_SIMULTANEOUS_CONN = 2
     JSON_API_CONN_KEY_NAME = 'herbs_json_connections'
-    JSON_API_CONN_MAX_TIME = 3600
+    JSON_API_CONN_KEY_FLAG = 'herbs_json_conn_flag'
+    JSON_API_CONN_TIMEOUT = 3
 
     class Meta:
         prefix = 'herbs'
