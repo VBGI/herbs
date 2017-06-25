fd0019dc59fbb1823ded440915f290d3022cdf5c
Dmitry E. Kislov <kislov@easydan.com>
2017-06-25 15:10:56 +1000

    Typos and fixes: bilingual support is deployed..

--- herbs/templatetags/herbs_tags.py
+++ herbs/templatetags/herbs_tags.py
@@ -3,6 +3,8 @@
 from django import template
 from django.conf import settings
 from django.utils import translation
+from ..models import Country
+
 
 register = template.Library()
 
@@ -13,6 +15,15 @@ CYRILLIC_SYMBOLS = set(u"абвгдезиклмнопрстуфхцЦъыьАБ�
 
 @register.filter
 def smart_language(value):
+    if isinstance(value, Country):
+        if translation.get_language() == 'ru':
+            return value.name_ru
+        else:
+            return value.name_en
+    try:
+        value = str(value)
+    except UnicodeEncodeError:
+        pass
     if DELIMITER in value:
         if value.count(DELIMITER) > 1:
             return value
@@ -21,8 +32,8 @@ def smart_language(value):
         b = b.strip()
         if len(a) == 0 or len(b) == 0:
             return value
-        _cya = len(set(a.decode('utf-8')).intersection(CYRILLIC_SYMBOLS))
-        _cyb = len(set(b.decode('utf-8')).intersection(CYRILLIC_SYMBOLS))
+        _cya = len(set(a).intersection(CYRILLIC_SYMBOLS))
+        _cyb = len(set(b).intersection(CYRILLIC_SYMBOLS))
         if _cya > _cyb:
             ru_value = a
             en_value = b
