09f1897da94a626ac6b1efc9f4051a12f655b0cc
Kislov Dmitry <kislov@easydan.com>
2017-12-05 17:21:09 +1000

    Image indexing policy: changed

--- herbs/management/process_images.py
+++ herbs/management/process_images.py
@@ -95,6 +95,26 @@ def get_acronym_name(x):
     return res[-1] if res else ''
 
 
+def check_image_exists(image_name):
+    '''Check for existence of the image
+
+    :param image_name:
+    :return: true if image_name already exists
+    '''
+    res = []
+    image_name = '.'.join(image_name.split('.')[:-1])
+    for subim in IMAGE_CONVERSION_OPTS:
+        destination_file = os.path.join(OUTPUT_IMAGE_PATH,
+                                    get_acronym_name(image_name),
+                                    subim, image_name + '.' +
+                                    IMAGE_CONVERSION_OPTS[subim]['format']
+                                    )
+        res.append(os.path.isfile(destination_file) and not IMAGE_CONVERSION_OPTS[subim]['overwrite'])
+    return all(res)
+
+
+
+
 def easy_process():
     source_images = list(get_all_image_files())
 
@@ -119,69 +139,71 @@ def easy_process():
         print('Copying the file:', imfile)
         bname = os.path.basename(imfile)
         tmp_image = os.path.join(TMP_FOLDER, bname)
-        shutil.copyfile(imfile, tmp_image, follow_symlinks=False)
-        tiffstack = Image.open(tmp_image)
-        if tiffstack.n_frames > 1:
-            tfw_array = []
-            tfw_frames = []
-            for k in range(tiffstack.n_frames):
-                try:
-                    tiffstack.seek(k)
-                    tfw_array.append(tiffstack.width)
-                    tfw_frames.append(k)
-                except EOFError:
-                    pass
-            tiffstack.seek(tfw_frames[np.argmax(tfw_array)])
-        else:
-            tiffstack.seek(0)
-        print('Appropriate tiff layer extracted...')
-        temp_image_name = bname.split('.')[0]
-        tiffstack.save(os.path.join(TMP_FOLDER, temp_image_name + '.png'))
-        print('Temporary image file is created: ', os.path.join(TMP_FOLDER,
-                                                                temp_image_name))
-        cmd_stack = ['convert']
-        cmd_stack.append(os.path.join(TMP_FOLDER, temp_image_name + '.png'))
 
-        # check if rotation needed
-        rotation = tiffstack.width >= tiffstack.height
-        tiffstack.close()
-
-        for subim in IMAGE_CONVERSION_OPTS:
-            destination_file = os.path.join(OUTPUT_IMAGE_PATH,
-                                            get_acronym_name(temp_image_name),
-                                            subim, temp_image_name + '.' +
-                                            IMAGE_CONVERSION_OPTS[subim]['format']
-                                            )
-            if not os.path.isfile(destination_file) or IMAGE_CONVERSION_OPTS[subim]['overwrite']:
-                cmd_stack_cur = cmd_stack.copy()
-                if rotation:
-                    cmd_stack_cur.append('-rotate')
-                    cmd_stack_cur.append('270')
-
-                if IMAGE_CONVERSION_OPTS[subim]['size']:
-                    cmd_stack_cur.append('-resize')
-                    cmd_stack_cur.append(IMAGE_CONVERSION_OPTS[subim]['size'])
-
-                if IMAGE_CONVERSION_OPTS[subim]['extra']:
-                    cmd_stack_cur.extend(IMAGE_CONVERSION_OPTS[subim]['extra'])
-
-
-                cmd_stack_cur.append(destination_file)
-                _p = subprocess.Popen(cmd_stack_cur)
-                _p.wait()
-                print('Generating ', subim, 'image for', temp_image_name)
+        if not check_image_exists(bname):
+            shutil.copyfile(imfile, tmp_image, follow_symlinks=False)
+            tiffstack = Image.open(tmp_image)
+            if tiffstack.n_frames > 1:
+                tfw_array = []
+                tfw_frames = []
+                for k in range(tiffstack.n_frames):
+                    try:
+                        tiffstack.seek(k)
+                        tfw_array.append(tiffstack.width)
+                        tfw_frames.append(k)
+                    except EOFError:
+                        pass
+                tiffstack.seek(tfw_frames[np.argmax(tfw_array)])
             else:
-                print("The file ", destination_file, "already exists.")
+                tiffstack.seek(0)
+            print('Appropriate tiff layer extracted...')
+            temp_image_name = bname.split('.')[0]
+            tiffstack.save(os.path.join(TMP_FOLDER, temp_image_name + '.png'))
+            print('Temporary image file is created: ', os.path.join(TMP_FOLDER,
+                                                                    temp_image_name))
+            cmd_stack = ['convert']
+            cmd_stack.append(os.path.join(TMP_FOLDER, temp_image_name + '.png'))
+
+            # check if rotation needed
+            rotation = tiffstack.width >= tiffstack.height
+            tiffstack.close()
+
+            for subim in IMAGE_CONVERSION_OPTS:
+                destination_file = os.path.join(OUTPUT_IMAGE_PATH,
+                                                get_acronym_name(temp_image_name),
+                                                subim, temp_image_name + '.' +
+                                                IMAGE_CONVERSION_OPTS[subim]['format']
+                                                )
+                if not os.path.isfile(destination_file) or IMAGE_CONVERSION_OPTS[subim]['overwrite']:
+                    cmd_stack_cur = cmd_stack.copy()
+                    if rotation:
+                        cmd_stack_cur.append('-rotate')
+                        cmd_stack_cur.append('270')
+
+                    if IMAGE_CONVERSION_OPTS[subim]['size']:
+                        cmd_stack_cur.append('-resize')
+                        cmd_stack_cur.append(IMAGE_CONVERSION_OPTS[subim]['size'])
+
+                    if IMAGE_CONVERSION_OPTS[subim]['extra']:
+                        cmd_stack_cur.extend(IMAGE_CONVERSION_OPTS[subim]['extra'])
 
 
-        try:
-            os.remove(os.path.join(TMP_FOLDER, temp_image_name + '.png'))
-            os.remove(tmp_image)
-            print('Temporary files were removed...')
-        except IOError:
-            pass
+                    cmd_stack_cur.append(destination_file)
+                    _p = subprocess.Popen(cmd_stack_cur)
+                    _p.wait()
+                    print('Generating ', subim, 'image for', temp_image_name)
+                else:
+                    print("The file ", destination_file, "already exists.")
 
 
+            try:
+                os.remove(os.path.join(TMP_FOLDER, temp_image_name + '.png'))
+                os.remove(tmp_image)
+                print('Temporary files were removed...')
+            except IOError:
+                pass
+        else:
+            print('File %s alredy exists' % bname)
 
 easy_process()
 
