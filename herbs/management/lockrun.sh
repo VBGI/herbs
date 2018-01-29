0e92a237fb94205d3ee73c24aaf2bbf9d2707b58
Kislov Dmitry <kislov@easydan.com>
2018-01-29 15:24:16 +1000

    locker var added

--- herbs/management/lockrun.sh
+++ herbs/management/lockrun.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+locker="/tmp/herb-locked"
 
 homed="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd $homed && flock -w 10 /tmp/herb-locked $homed/run.sh
+cd $homed && flock -w 10 $locker $homed/run.sh
 
+
+sleep 5m
+rm $locker
